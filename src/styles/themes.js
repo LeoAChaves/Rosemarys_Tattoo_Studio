9de0f165613b10959c8aba0acadeb7c90303c0fd
LeoAChaves <leochaves2121@gmail.com>
2022-04-21 18:30:19 -0300

    Fazendo testes de mudança de tema

---
+++ src/styles/themes.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  body: "#ffffff",
+  h1: "#000000",
+  h2: "#000111",
+  h3: "#333333",
+  h4: "#666666",
+  h5: "#888888",
+  h6: "#bbbbbb",
+};
+
+export const darkTheme = {
+  body: "#000000",
+  h1: "#ffffff",
+  h2: "#bbbbbb",
+  h3: "#888888",
+  h4: "#666666",
+  h5: "#333333",
+  h6: "#000111",
+};
