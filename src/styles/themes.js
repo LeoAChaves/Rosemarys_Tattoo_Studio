8a62cbfcf6b4b59f1cafd0db6502eb61aadff7f9
alvesarah <sarah.alves8@hotmail.com>
2022-04-22 07:28:50 -0300

    Inserção de cor padrão

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,15 +1,23 @@
+const branco_rosado_opaco = "#e5e1d2b6";
+
 export const lightTheme = {
-  body: "#ffffff",
+  body: "#FDFDFD",
   primary: "#E5E1D280",
-  text: "#7B0000",
   button: "#FFB930",
   button_text: "#282320",
   input: "#FDFDFD",
-  input_placeholder: "#7B0000",
   card: "#FDFDFD",
+  text: "#7B0000",
+  input_placeholder: "#7B0000",
   card_border: "#7B0000",
   card_text: "#7B0000",
   card_info: "#5E5A57",
+  // vermelho: "#7B0000",
+  // preto: "#0A0807",
+  // amarelo: "#FFB930",
+  // branco: "#FDFDFD",
+  // cinza: "#E5E1D280",
+  branco_rosado_opaco,
 };
 
 export const darkTheme = {
@@ -24,4 +32,5 @@ export const darkTheme = {
   card_border: "#000000",
   card_text: "#000000",
   card_info: "#5E5A57",
+  branco_rosado_opaco,
 };
