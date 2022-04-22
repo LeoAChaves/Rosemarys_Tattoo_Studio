62ddaf34395e55b87ea431510f56e88dde1d12f8
thaihc <thaisherculanodecarvalho@gmail.com>
2022-04-22 19:40:19 -0300

    criando a pag estoque

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/themes.js";
 import { GlobalStyle } from "./styles/global.js";
 import FHome from "./pages/Funcionario/FHome.jsx";
+import FEstoque from "./pages/Funcionario/FEstoque.jsx";
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Router>
         <Routes>
           <Route path="/funcionario/home" element={<FHome />} />
+          <Route path="/funcionario/estoque" element={<FEstoque />} />
         </Routes>
       </Router>
     </ThemeProvider>
