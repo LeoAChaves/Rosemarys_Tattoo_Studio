785616fc796f2f935f90123ffb7f6d71930af8ae
alvesarah <sarah.alves8@hotmail.com>
2022-04-22 07:27:16 -0300

    Criando rota para login

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/themes.js";
 import { GlobalStyle } from "./styles/global.js";
 import FHome from "./pages/Funcionario/FHome.jsx";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <Router>
         <Routes>
           <Route path="/funcionario/home" element={<FHome />} />
+          <Route path="/login" element={<Login />}/>
         </Routes>
       </Router>
     </ThemeProvider>
