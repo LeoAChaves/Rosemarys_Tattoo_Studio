e54dc845a1fbfeae5226ad59d05826dbaf4dc7ae
thaihc <thaisherculanodecarvalho@gmail.com>
2022-04-23 00:07:38 -0300

    criando as pags funcionarios

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import FAgendamento from "./pages/Funcionario/FAgendamento.jsx";
 
 function App() {
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <Router>
         <Routes>
