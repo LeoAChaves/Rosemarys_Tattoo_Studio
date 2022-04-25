e06bec88a4b451ef170250e5fa294bcce04ed1b8
LeoAChaves <leochaves2121@gmail.com>
2022-04-25 14:35:58 -0300

    Adicionando Services para API Estoque

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,11 @@ import axios from "axios";
 export const apiCliente = axios.create({
   baseURL: "https://tatto-cliente.herokuapp.com/",
 });
+
 export const apiAgenda = axios.create({
   baseURL: "https://rosemary-api-leo.herokuapp.com",
+});
+
+export const apiEstoque = axios.create({
+  baseURL: "https://rest-api-rosemarys-estoque.herokuapp.com",
 });
