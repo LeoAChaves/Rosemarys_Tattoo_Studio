ff359f4dae4d8a5435c21dde4c8668d342737f26
Mauriciiow <mauricioliveira639@gmail.com>
2022-04-24 23:17:50 -0300

    corrigindo data que vem errada da api

--- src/pages/Usuario/index.jsx
+++ src/pages/Usuario/index.jsx
@@ -1,20 +1,46 @@
 import Moldura from "../../assets/molduraDadosUsuario.png";
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header";
 import FormUsuario from "../../components/Form/FormUsuario";
 import Paragrafo from "../../components/Paragrafo";
 import Subtitulo from "../../components/Subtitulo";
 import Button from "../../components/Button";
 import Footer from "../../components/Footer";
+import { useNavigate } from "react-router-dom";
 
 import * as S from "./styled";
+import { apiCliente } from "../../services/api";
 
 function Usuario({changeTheme}){
     const style = {
         height: "70px",
     }
 
-    
+    const {id} = useParams()
+    const navigate = useNavigate()
+    const [usuario, setUsuario] = useState({})
+    const [load, setLoad] = useState(true)
+    useEffect(() => {
+        async function buscaUsuario(){
+            try {
+              const response = await apiCliente.get(`/cliente/${id}`)
+                setLoad(false)
+                setUsuario(response.data.cliente)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        buscaUsuario()
+    }, []);
+
+    const dataCorreta= (data)=>{
+        const dataArr = data.split('-')
+        const ano = dataArr.shift()
+        const dia = dataArr.pop()
+       
+        return `${dia}-${dataArr.pop()}-${ano}`
+    }
 
     return(
         <>
@@ -32,13 +58,13 @@ function Usuario({changeTheme}){
                         <img src={Moldura} alt="Moldura" />
                         <div>
                             <h2>Seus dados</h2>
-                            <Paragrafo texto="Nome completo:" atributo="" />
-                            <Paragrafo texto="Data de nascimento:" atributo="" />
-                            <Paragrafo texto="Gênero:" atributo="" />
-                            <Paragrafo texto="CPF:" atributo="" />
-                            <Paragrafo texto="Email:" atributo="" />
+                            <Paragrafo texto="Nome completo:" atributo={usuario.nome} />
+                            <Paragrafo texto="Data de nascimento:" atributo={load === false ? dataCorreta(usuario.data_nascimento) : ''}/>
+                            <Paragrafo texto="Gênero:" atributo={usuario.genero} />
+                            <Paragrafo texto="CPF:" atributo={usuario.cpf} />
+                            <Paragrafo texto="Email:" atributo={usuario.email} />
                 
-                            <Button className="btnAlterar" nome="Alterar" />
+                            <Button className="btnAlterar" nome="Alterar" onClick={()=>navigate(`/atualizarUsuario/${id}`)}/>
                         </div>
                     </section>
 
