adbda110dcbdbdea7a5e8f53c0210a861384c6eb
LeoAChaves <leochaves2121@gmail.com>
2022-04-25 09:23:07 -0300

    Inserindo Services "apiAgenda"

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const apiCliente = axios.create({
-    baseURL: 'https://tatto-cliente.herokuapp.com/'
-})
-
+  baseURL: "https://tatto-cliente.herokuapp.com/",
+});
+export const apiAgenda = axios.create({
+  baseURL: "https://rosemary-api-leo.herokuapp.com",
+});
