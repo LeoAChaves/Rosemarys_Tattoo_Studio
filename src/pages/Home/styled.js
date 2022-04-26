8d11407836f59ff2c2393d5ec33db36e9d5e87e8
lesantxs <l.sancarmo@gmail.com>
2022-04-26 15:20:40 -0300

    Adicionando divs estilizadas, mudança de cor de fundo e tamanho variados

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -36,15 +36,16 @@ export const BlocoDois = styled.div`
 export const Line = styled.div`
   display: flex;
   align-items: center;
-  width: 50%;
-  height: 1px;
+  width: 30%;
+  height: 2px;
   background-color: ${({ theme }) => theme.text};
+  margin: 2px 0 7px 0;
 `;
 
 export const Line2 = styled.div`
   display: flex;
   align-items: center;
-  width: 30%;
+  width: 50%;
   height: 1px;
   background-color: ${({ theme }) => theme.text};
 `;
@@ -74,12 +75,12 @@ export const Paragrafo = styled.p`
 `;
 
 export const DivImage = styled.div`
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
   img {
     width: 250px;
     height: 309px;
-    border-radius: 20px;
+    border-radius: 5px;
     margin: 20px;
   }
 `;
@@ -90,30 +91,48 @@ export const BlocoTres = styled.div`
   align-items: center;
   height: auto;
   width: 100%;
-  background-color: ${({ theme }) => theme.homeBG};
+  background-color: ${({ theme }) => theme.homeBGSec};
   padding: 20px;
 `;
 
 export const Time = styled.div`
-display: grid;
-grid-template-columns: repeat(4, 4fr);
+  display: grid;
+  grid-template-columns: repeat(3, 3fr);
+`;
+
+export const Artista = styled.div`
+margin: 30px 30px 100px 30px;
+text-align: center;
 img {
     width: 250px;
     height: 250px;
-    border-radius: 50%;
-    margin: 30px 30px 100px 30px;
+    border-radius: 3px;
+    margin-bottom: 5px;
   }
-
 `
 
 export const BlocoQuatro = styled.div`
-  height: 100vh;
+   display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: auto;
   width: 100%;
   background-color: ${({ theme }) => theme.homeBG};
+  padding: 20px;
+`;
+
+export const Trabalhos = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 4fr);
+  img {
+    height: 300px;
+    width: 335px;
+  }
+  margin: 70px 0 70px 0;
 `;
 
 export const BlocoCinco = styled.div`
   height: 100vh;
   width: 100%;
-  background-color: ${({ theme }) => theme.homeBG};
+  background: url(${({ theme }) => theme.background}) no-repeat bottom center scroll;
 `;
