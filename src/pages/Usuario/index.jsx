4665072a8fa5ba402b1d2ea8f3f8c6b4845fe412
lesantxs <l.sancarmo@gmail.com>
2022-04-25 23:39:20 -0300

    Merge branch 'SarahAlves' into LeSantos

--- src/pages/Usuario/index.jsx
+++ src/pages/Usuario/index.jsx
@@ -1,9 +1,5 @@
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
-import { useParams,useNavigate } from "react-router-dom";
-=======
 import { useParams } from "react-router-dom";
->>>>>>> LeoChaves
 import Header from "../../components/Header";
 import FormUsuario from "../../components/Form/FormUsuario";
 import Paragrafo from "../../components/Paragrafo";
@@ -36,64 +32,14 @@ function Usuario({ changeTheme }) {
     buscaUsuario();
   }, []);
 
-<<<<<<< HEAD
-    const {id} = useParams()
-    const navigate = useNavigate()
-    const [usuario, setUsuario] = useState({})
-    const [load, setLoad] = useState(true)
-    useEffect(() => {
-        async function buscaUsuario(){
-            try {
-              const response = await apiCliente.get(`/cliente/${id}`)
-                setLoad(false)
-                setUsuario(response.data.cliente)
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        buscaUsuario()
-    }, []);
-=======
   const dataCorreta = (data) => {
     const dataArr = data.split("-");
     const ano = dataArr.shift();
     const dia = dataArr.pop();
->>>>>>> LeoChaves
 
     return `${dia}-${dataArr.pop()}-${ano}`;
   };
 
-<<<<<<< HEAD
-    return(
-        <>
-            <Header style={style} div={{display:"none"}} portfolio="Portifólio" sair="Sair" changeTheme={changeTheme} />
-            <S.Main>
-                <S.BlocoUm>
-                    <Subtitulo texto="Bem vindo de volta {nome}" nome="" />
-                    <section>
-                        <p>Você não tem agendamentos disponíveis</p>
-                    </section>
-                </S.BlocoUm>
-                <S.BlocoDois>
-                    <FormUsuario />
-                    <section>                            
-                        <div className="container">
-                            <div className="bordaAnimada"></div>
-                            <div className="corner">
-                                <div>
-                                    <h2>Seus dados</h2>
-                                    <Paragrafo texto="Nome completo:" atributo={usuario.nome} />
-                                    <Paragrafo texto="Data de nascimento:" atributo={load === false ? dataCorreta(usuario.data_nascimento) : ''}/>
-                                    <Paragrafo texto="Gênero:" atributo={usuario.genero} />
-                                    <Paragrafo texto="CPF:" atributo={usuario.cpf} />
-                                    <Paragrafo texto="Email:" atributo={usuario.email} />
-                        
-                                    <Button className="btnAlterar" nome="Alterar" onClick={()=>navigate(`/atualizarUsuario/${id}`)}/>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-=======
   return (
     <>
       <Header
@@ -128,7 +74,6 @@ function Usuario({ changeTheme }) {
                   <Paragrafo texto="Gênero:" atributo={usuario.genero} />
                   <Paragrafo texto="CPF:" atributo={usuario.cpf} />
                   <Paragrafo texto="Email:" atributo={usuario.email} />
->>>>>>> LeoChaves
 
                   <Button
                     className="btnAlterar"
