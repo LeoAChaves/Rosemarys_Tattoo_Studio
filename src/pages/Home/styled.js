ba07ca634fdb5a152bfed1f0e6a7d60e922886d6
lesantxs <l.sancarmo@gmail.com>
2022-04-26 18:36:29 -0300

    Adicionando estilização de icon, novas configuações de parágrafo e do bloco cinco, alterações de cor de fundo

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -29,9 +29,14 @@ export const BlocoDois = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: ${({ theme }) => theme.homeBG};
+  background-color: ${({ theme }) => theme.primary};
   padding: 20px;
 `;
+
+export const IconFD = styled.div`
+font-size: 45px;
+margin-top: 28px;
+`
 
 export const Line = styled.div`
   display: flex;
@@ -69,9 +74,26 @@ export const H1 = styled.h1`
   font-size: 50px;
 `;
 
+export const H2 = styled.h1`
+  font-size: 35px;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+`;
+
 export const Paragrafo = styled.p`
   font-size: 16px;
   width: 300px;
+`;
+
+export const Paragrafo2 = styled.p`
+  font-size: 20px;
+  width: 300px;
+  color: ${({ theme }) => theme.branco_rosado_opaco};
+`;
+
+export const Paragrafo3 = styled.p`
+  font-size: 20px;
+  color: ${({ theme }) => theme.text};
 `;
 
 export const DivImage = styled.div`
@@ -91,7 +113,7 @@ export const BlocoTres = styled.div`
   align-items: center;
   height: auto;
   width: 100%;
-  background-color: ${({ theme }) => theme.homeBGSec};
+  background-color: ${({ theme }) => theme.preto};
   padding: 20px;
 `;
 
@@ -108,17 +130,22 @@ img {
     height: 250px;
     border-radius: 3px;
     margin-bottom: 5px;
+    cursor: pointer;
   }
+  border: 2px solid ${({ theme }) => theme.branco_rosado_opaco};
+  padding: 20px 0 15px 0;
+  border-radius: 5px;
 `
 
 export const BlocoQuatro = styled.div`
    display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   height: auto;
   width: 100%;
-  background-color: ${({ theme }) => theme.homeBG};
-  padding: 20px;
+  background-color: ${({ theme }) => theme.primary};
+  padding-top: 20px;
 `;
 
 export const Trabalhos = styled.div`
@@ -126,13 +153,20 @@ export const Trabalhos = styled.div`
   grid-template-columns: repeat(4, 4fr);
   img {
     height: 300px;
-    width: 335px;
+    width: 100%;
+    cursor: pointer;
   }
-  margin: 70px 0 70px 0;
+  margin-top: 45px;
 `;
 
 export const BlocoCinco = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
   height: 100vh;
   width: 100%;
   background: url(${({ theme }) => theme.background}) no-repeat bottom center scroll;
+  padding: 25px;
 `;
