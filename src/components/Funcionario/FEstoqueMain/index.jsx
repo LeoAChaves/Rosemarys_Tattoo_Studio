34a544be61f0a9ca4ad78847e4a6969f3f732c4d
LeoAChaves <leochaves2121@gmail.com>
2022-04-27 17:35:17 -0300

    Gerando dinamicamente itens de Estoque

--- src/components/Funcionario/FEstoqueMain/index.jsx
+++ src/components/Funcionario/FEstoqueMain/index.jsx
@@ -2,97 +2,80 @@ import * as S from "./styled.js";
 import Button from "../../Button/index.jsx";
 import Input from "../../Input/index.jsx";
 
+import { apiEstoque } from "../../../services/api.js";
+import { useEffect, useState } from "react";
+import Carregando from "../../Carregando";
+import toast from "react-hot-toast";
+
 function FEstoqueMain() {
+  const [estoque, setEstoque] = useState([]);
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+    async function getEstoque() {
+      try {
+        const response = await apiEstoque.get("/estoque");
+        setEstoque(response.data.estoque);
+        setLoad(false);
+      } catch (error) {
+        toast.error(error.response.data);
+      }
+    }
+    getEstoque();
+  }, []);
+
   return (
-    <S.Container>
-      <S.Quadro>
-        <h2>Estoque</h2>
-        <S.Form>
-          <div class="busca">
-            <Input
-              placeholder="palavra-chave"
-              type="text"
-              name="search"
-              id="search"
-            ></Input>
-            <Button type="submit" nome="Buscar"></Button>
-          </div>
-          <S.Cards>
-            <div class="lista">
-              <ul>
-                <li>ID:</li>
-                <li>ITEM:</li>
-                <li>PREÇO:</li>
-                <li>QUANTIDADE:</li>
-                <li>TIPO:</li>
-                <div className="botao">
-                  <Button
-                    className="styleForm"
-                    type="submit"
-                    nome="Alterar"
-                    //onClick={(e) => alterarEstoque(e)}
-                  ></Button>
-                  <Button
-                    className="styleForm"
-                    type="submit"
-                    nome="Deletar"
-                    //onClick={(e) => deletarEstoque(e)}
-                  ></Button>
-                </div>
-              </ul>
-            </div>
-
-            <div class="lista">
-              <ul>
-                <li>ID:</li>
-                <li>ITEM:</li>
-                <li>PREÇO:</li>
-                <li>QUANTIDADE:</li>
-                <li>TIPO:</li>
-                <div className="botao">
-                  <Button
-                    className="styleForm"
-                    type="submit"
-                    nome="Alterar"
-                    //onClick={(e) => alterarEstoque(e)}
-                  ></Button>
-                  <Button
-                    className="styleForm"
-                    type="submit"
-                    nome="Deletar"
-                    //onClick={(e) => deletarEstoque(e)}
-                  ></Button>
-                </div>
-              </ul>
-            </div>
-
-            <div class="lista">
-              <ul>
-                <li>ID:</li>
-                <li>ITEM:</li>
-                <li>PREÇO:</li>
-                <li>QUANTIDADE:</li>
-                <li>TIPO:</li>
-                <div className="botao">
-                  <Button
-                    className="styleForm"
-                    type="submit"
-                    nome="Alterar"
-                    //onClick={(e) => alterarEstoque(e)}
-                  ></Button>
-                  <Button
-                    className="styleForm"
-                    type="submit"
-                    nome="Deletar"
-                    //onClick={(e) => deletarEstoque(e)}
-                  ></Button>
-                </div>
-              </ul>
-            </div>
-          </S.Cards>
-        </S.Form>
-      </S.Quadro>
-    </S.Container>
+    <>
+      {load ? (
+        <Carregando />
+      ) : (
+        <S.Container>
+          <S.Quadro>
+            <h2>Estoque</h2>
+            <S.Form>
+              <div class="busca">
+                <Input
+                  placeholder="palavra-chave"
+                  type="text"
+                  name="search"
+                  id="search"
+                ></Input>
+                <Button type="submit" nome="Buscar"></Button>
+              </div>
+              <S.Cards>
+                {estoque.map((estoque) => {
+                  return (
+                    <div class="lista" key={estoque.ID}>
+                      <ul>
+                        <li>ID:{estoque.ID}</li>
+                        <li>ITEM:{estoque.NOME}</li>
+                        <li>PREÇO:{estoque.PRECO}</li>
+                        <li>QUANTIDADE:{estoque.QUANTIDADE}</li>
+                        <li>TIPO:{estoque.TIPO}</li>
+                        <div className="botao">
+                          <Button
+                            className="styleForm"
+                            type="submit"
+                            nome="Alterar"
+                            //onClick={(e) => alterarEstoque(e)}
+                          ></Button>
+                          <Button
+                            className="styleForm"
+                            type="submit"
+                            nome="Deletar"
+                            //onClick={(e) => deletarEstoque(e)}
+                          ></Button>
+                        </div>
+                      </ul>
+                    </div>
+                  );
+                })}
+              </S.Cards>
+            </S.Form>
+          </S.Quadro>
+        </S.Container>
+      )}
+    </>
   );
 }
 export default FEstoqueMain;
