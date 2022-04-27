1d32f665327a422559759da6083e63524affe20e
Mauriciiow <mauricioliveira639@gmail.com>
2022-04-26 23:03:14 -0300

    criando get para api do portfolio

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,19 +1,39 @@
 import * as S from "./styled.js";
-
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Image from "../../components/Image";
 import Footer from "../../components/Footer";
 import ImagemGaleria from "../../components/ImagemGaleria";
+import { apiPortifolio } from "../../services/api.js";
 
 function Portfolio({changeTheme}) {
     const style = {
         height: "70px",
     }
+    const [portfolio, setPortifolio] = useState([])
+
+    useEffect(() => {
+        async function getPortfolios() {
+            try {
+                const response = await apiPortifolio.get('/portfolio')
+                setPortifolio(response.data.portfolios)
+            
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getPortfolios()
+    }, []);
     return (
         <div>
             <Header btnVoltar={{display: "none"}} btnInicial={{display: "none"}} btnPortfolio={{display: "none"}} btnLogin={{display: "none"}} btnSair={{display: "none"}} className={"btn custom-btn styleHeader"} style={style} div={{display:"none"}} home="Home" changeTheme={changeTheme}/>
             <S.Main>
-                <ImagemGaleria href="#1" alt="Cachorro fofinho" descricao="aaaaa" src="https://images-ext-2.discordapp.net/external/wrlV2sCdYGTN2lWZVRe1wa3xbYZKVHgx8c5olCSgBfY/%3Fquality%3D90%26strip%3Dinfo%26resize%3D680%2C453/https/super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png?width=678&height=452" />
+                {portfolio.map(portfolio=>{
+                   return <ImagemGaleria id={portfolio.ID} href={'#'+portfolio.ID} alt={portfolio.DESCRICAO} descricao={portfolio.DESCRICAO} src={portfolio.FOTO} />
+                })
+
+                }
+              
             </S.Main>
             <Footer />
         </div>
