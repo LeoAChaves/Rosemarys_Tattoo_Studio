96d961171aa2a3d05e1beef15d64c1ae2fd08802
Mauriciiow <mauricioliveira639@gmail.com>
2022-04-26 22:27:13 -0300

    criei const da api do portifolio

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,7 @@ export const apiCliente = axios.create({
     baseURL: 'https://tatto-cliente.herokuapp.com/'
 })
 
+export const apiPortifolio = axios.create({
+    baseURL: 'https://rest-api-rosemarys-portfolio.herokuapp.com/'
+})
+
