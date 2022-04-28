bc4845574949605fee0b85b008689c7f89182cb1
alvesarah <sarah.alves8@hotmail.com>
2022-04-28 04:41:19 -0300

    Header funcionario

---
+++ src/components/Funcionario/FHeader/index.jsx
@@ -0,0 +1,71 @@
+import { useNavigate } from "react-router-dom";
+
+import * as S from "./styled.js";
+
+import Paragrafo from "../../Paragrafo";
+import Button from "../../Button";
+import Image from "../../Image";
+
+import { VscColorMode } from "react-icons/vsc";
+
+function Header({
+  style,
+  div,
+  btnVoltar,
+  btnHome,
+  btnLogin,
+  btnSair,
+  className,
+  sair,
+  home,
+  login,
+  voltar,
+  changeTheme,
+}) {
+  const navigate = useNavigate();
+  return (
+    <S.Header style={style}>
+      <div>
+        <Paragrafo texto="ID:" atributo="" />
+        <Paragrafo texto="FUNCIONÁRIO:" atributo="" />
+        <Paragrafo texto="FUNÇÃO:" atributo="" />
+        <Paragrafo texto="STATUS:" atributo="" />
+      </div>
+      <div>
+        <Image
+          src="https://media.discordapp.net/attachments/968220165194014750/968303712965636156/unknown__2_-removebg-preview.png"
+          alt="logo"
+        />
+      </div>
+      <div className="divLinks">
+        <Button
+          style={btnHome}
+          className={className}
+          nome={home}
+          onClick={() => navigate(`/home`)}
+        />
+        <Button
+          style={btnLogin}
+          className={className}
+          nome={login}
+          onClick={() => navigate(`/login`)}
+        />
+        <Button
+          style={btnVoltar}
+          className={className}
+          nome={voltar}
+          onClick={() => navigate("/funcionario/home")}
+        />
+        <Button
+          style={btnSair}
+          className={className}
+          nome={sair}
+          onClick={() => navigate(`/login`)}
+        />
+        <VscColorMode className="themeMode" onClick={changeTheme} />
+      </div>
+    </S.Header>
+  );
+}
+
+export default Header;
---
+++ src/components/Funcionario/FHeader/styled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background-color: ${({ theme }) => theme.primary};
+  height: ${(props) => props.style.height};
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 30px;
+
+  img {
+    width: 65px;
+  }
+
+  .divLinks {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .divLinks > button {
+    margin: 0 20px;
+
+    cursor: pointer;
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+    color: ${({ theme }) => theme.text};
+  }
+
+  .themeMode {
+    margin: 0 20px;
+    cursor: pointer;
+    color: ${({ theme }) => theme.text};
+  }
+`;
