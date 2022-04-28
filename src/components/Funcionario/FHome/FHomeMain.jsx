3976b82b3c1443c916f05d32fe9316c1f8cba30e
Mauriciiow <mauricioliveira639@gmail.com>
2022-04-28 06:22:12 -0300

    chamando hook de funcionario

--- src/components/Funcionario/FHome/FHomeMain.jsx
+++ src/components/Funcionario/FHome/FHomeMain.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import FHomeCard from "./FHomeCard.jsx";
 import { useNavigate } from "react-router-dom";
+import useFuncionario from "../../Hooks/funcionario.jsx";
 
 function FHomeMain() {
   const navigate = useNavigate();
@@ -8,25 +9,26 @@ function FHomeMain() {
     width: 100%;
     height: calc(100vh - 80px);
   `;
+  const [funcionario] = useFuncionario()
   return (
     <FHomeMain>
       <FHomeCard
         tipo="agendamento"
         texto="Agendamento"
-        onClickVisu={() => navigate("/funcionario/agendamento-view")}
-        onClickInse={() => navigate("/funcionario/agendamento")}
+        onClickVisu={() => navigate("/funcionario/agendamento-view/"+funcionario.ID)}
+        onClickInse={() => navigate("/funcionario/agendamento/"+funcionario.ID)}
       />
       <FHomeCard
         tipo="portfolio"
         texto="Portifólio"
-        onClickVisu={() => navigate("/funcionario/portfolio-view")}
-        onClickInse={() => navigate("/funcionario/portfolio")}
+        onClickVisu={() => navigate("/funcionario/portfolio-view/"+funcionario.ID)}
+        onClickInse={() => navigate("/funcionario/portfolio/"+funcionario.ID)}
       />
       <FHomeCard
         tipo="estoque"
         texto="Estoque"
-        onClickVisu={() => navigate("/funcionario/estoque-view")}
-        onClickInse={() => navigate("/funcionario/estoque")}
+        onClickVisu={() => navigate("/funcionario/estoque-view/"+funcionario.ID)}
+        onClickInse={() => navigate("/funcionario/estoque/"+funcionario.ID)}
       />
     </FHomeMain>
   );
