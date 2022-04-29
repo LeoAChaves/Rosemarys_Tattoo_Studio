586d0f08a2c9a96c5dee4e496020c301aa164967
Mauriciiow <mauricioliveira639@gmail.com>
2022-04-29 04:14:29 -0300

    state para saber cor e mudar logo

--- src/components/Funcionario/FHeader/index.jsx
+++ src/components/Funcionario/FHeader/index.jsx
@@ -8,6 +8,7 @@ import Image from "../../Image";
 
 import { VscColorMode } from "react-icons/vsc";
 import useFuncionario from "../../Hooks/funcionario.jsx";
+import { useState } from "react";
 function Header({
   style,
   btnVoltar,
@@ -21,8 +22,13 @@ function Header({
 }) {
   const navigate = useNavigate();
   const [funcionario] = useFuncionario();
+  const [cor, setCor] = useState('#000000')
+
+
   return (
+    
     <S.Header style={style}>
+      
       <div className="dadosFunc">
         <Paragrafo texto="ID:" atributo={funcionario.ID} />
         <Paragrafo texto="FUNCIONÁRIO:" atributo={funcionario.NOME} />
@@ -30,26 +36,28 @@ function Header({
         <Paragrafo texto="STATUS:" atributo={funcionario.STATUS} />
       </div>
       <div>
-        {/* <Image
+      
+      {cor === '#000000' ? <Image
           className="logoGeral"
           src="https://media.discordapp.net/attachments/923686382659059742/969457766894813224/logoDark.png"
           alt="logoDark"
-        /> */}
+        /> :
         <Image
           className="logoGeral"
           src="https://media.discordapp.net/attachments/923686382659059742/969457767385563156/logoLight.png"
           alt="logoLight"
-        />
+        />}
         {/* <Image
           className="logoResponsivo"
           src="https://media.discordapp.net/attachments/923686382659059742/969457767175843910/logoDarkR.png?width=670&height=670"
           alt="logoDarkR"
         /> */}
-        <Image
+        {/* <Image
           className="logoResponsivo"
           src="https://media.discordapp.net/attachments/923686382659059742/969457767582687323/logoLightR.png?width=670&height=670"
           alt="logoLightR"
-        />
+        /> */}
+        
       </div>
       <div className="divLinks">
         <Button
@@ -70,7 +78,9 @@ function Header({
           nome={sair}
           onClick={() => navigate(`/login`)}
         />
-        <VscColorMode className="themeMode" onClick={changeTheme} />
+        
+     
+        <VscColorMode className="themeMode" onClick={()=>setCor(changeTheme())} />
       </div>
     </S.Header>
   );
