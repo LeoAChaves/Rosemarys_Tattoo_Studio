c73ff65b6d30bcad7eae68ace8863200a8bcf392
lesantxs <l.sancarmo@gmail.com>
2022-04-29 07:00:55 -0300

    Organizando código

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -5,64 +5,53 @@ const fadeAnime = keyframes`${fadeIn}`;
 
 export const Main = styled.main`
   color: ${({ theme }) => theme.text};
-
   height: auto;
   width: 100%;
-
   background-position: 30% 0%;
   background-size: cover;
 
-  overflow-x: hidden;
-
-
-  @media(max-width: 1200px){
-    
-      .respo{
-        display: flex;
-             flex-direction: column;
-             justify-content: space-between;
-              height: auto;
+  @media (max-width: 1200px) {
+    .respo {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      height: auto;
     }
 
-    .respoDiv{
+    .respoDiv {
       display: grid;
       grid-template-columns: repeat(1, 1fr);
       height: auto;
     }
 
-    .respoTimeTrab{
+    .respoTimeTrab {
       display: grid;
-      grid-template-columns: repeat(2, 2fr)
+      grid-template-columns: repeat(2, 2fr);
     }
   }
 
-
-
-    @media(max-width: 850px){
-      .respoTimeTrab{
+  @media (max-width: 850px) {
+    .respoTimeTrab {
       display: grid;
-      grid-template-columns: repeat(1, 1fr)
+      grid-template-columns: repeat(1, 1fr);
     }
-    }
+  }
 
-    @media (max-width: 600px){
-      .socorro{
-        display: flex;
-        flex-direction: column;
-      }
-    }    
-`
+  @media (max-width: 600px) {
+    .socorro {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`;
 
 export const BlocoUm = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-
-  background: url(${({ theme }) => theme.background}) no-repeat bottom center
-    scroll;
+  background: url(${({ theme }) => theme.background}) no-repeat bottom center scroll;
   background-position: 30% 0%;
   background-size: cover;
-
   height: 100vh;
   width: 100%;
 
@@ -70,7 +59,6 @@ export const BlocoUm = styled.div`
     padding-left: 80px;
     animation: 1s ${rotateAnima};
   }
-
   .heroText {
     font-size: 75px;
     text-transform: uppercase;
@@ -85,28 +73,12 @@ export const BlocoDois = styled.div`
   align-items: center;
   background-color: ${({ theme }) => theme.primary};
   padding: 20px;
+  overflow-x: hidden;
 `;
 
 export const IconFD = styled.div`
   font-size: 45px;
   margin-top: 28px;
-`;
-
-export const Line = styled.div`
-  display: flex;
-  align-items: center;
-  width: 30%;
-  height: 2px;
-  background-color: ${({ theme }) => theme.text};
-  margin: 2px 0 7px 0;
-`;
-
-export const Line2 = styled.div`
-  display: flex;
-  align-items: center;
-  width: 50%;
-  height: 1px;
-  background-color: ${({ theme }) => theme.text};
 `;
 
 export const DivCard = styled.div`
@@ -125,34 +97,6 @@ export const DivTexto = styled.div`
   height: 100%;
 `;
 
-export const H1 = styled.h1`
-  font-size: 50px;
-`;
-
-export const H2 = styled.h2`
-  font-size: 35px;
-  text-transform: uppercase;
-  margin-bottom: 5px;
-`;
-
-export const Paragrafo = styled.p`
-  font-size: 16px;
-  width: 300px;
-`;
-
-export const H3 = styled.h3`
-  font-size: 20px;
-  width: 300px;
-  color: ${({ theme }) => theme.branco_opaco};
-  position: relative;
-  z-index: 1000;
-`;
-
-export const Paragrafo2 = styled.p`
-  font-size: 20px;
-  color: ${({ theme }) => theme.text};
-`;
-
 export const DivImage = styled.div`
   display: flex;
   align-items: center;
@@ -166,6 +110,51 @@ export const DivImage = styled.div`
     object-fit: cover;
     border: 3px solid ${({ theme }) => theme.text};
   }
+`;
+
+export const H1 = styled.h1`
+  font-size: 50px;
+`;
+
+export const H2 = styled.h2`
+  font-size: 35px;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+`;
+
+export const H3 = styled.h3`
+  font-size: 20px;
+  width: 300px;
+  color: ${({ theme }) => theme.branco_opaco};
+  position: relative;
+  z-index: 1000;
+`;
+
+export const Paragrafo = styled.p`
+  font-size: 16px;
+  width: 300px;
+`;
+
+export const Paragrafo2 = styled.p`
+  font-size: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+export const Line = styled.div`
+  display: flex;
+  align-items: center;
+  width: 30%;
+  height: 2px;
+  background-color: ${({ theme }) => theme.text};
+  margin: 2px 0 7px 0;
+`;
+
+export const Line2 = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.text};
 `;
 
 export const BlocoTres = styled.div`
@@ -188,6 +177,10 @@ export const Artista = styled.div`
   margin: 50px 30px 50px 30px;
   text-align: center;
   cursor: pointer;
+  border: 2px solid ${({ theme }) => theme.branco_rosado_opaco};
+  padding: 20px 20px 15px 20px;
+  border-radius: 5px;
+  transition: all 1s ease;
   img {
     width: 250px;
     height: 400px;
@@ -201,18 +194,10 @@ export const Artista = styled.div`
     -webkit-filter: blur(2px); /* Chrome, Safari, Opera */
     filter: blur(2px);
   }
-
   div {
     display: none;
   }
-
-  border: 2px solid ${({ theme }) => theme.branco_rosado_opaco};
-  padding: 20px 20px 15px 20px;
-  border-radius: 5px;
-  transition: all 1s ease;
-
   &:hover div {
-    display: block;
     position: absolute;
     width: 100%;
     height: 100%;
@@ -244,6 +229,7 @@ export const BlocoQuatro = styled.div`
 export const Trabalhos = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 4fr);
+  margin-top: 45px;
   .trabAnima {
     height: 300px;
     width: 100%;
@@ -255,12 +241,10 @@ export const Trabalhos = styled.div`
       transition: all 1s ease;
     }
   }
-
   .fotoTrab:hover {
     opacity: 60%;
     transition: all 1s ease;
   }
-  margin-top: 45px;
 `;
 
 export const BlocoCinco = styled.div`
@@ -275,6 +259,7 @@ export const BlocoCinco = styled.div`
   background-position: 30% 0%;
   background-size: cover;
   padding: 25px;
+  overflow-x: hidden;
 `;
 
 export const Form = styled.form`
